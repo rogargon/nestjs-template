f297e029554ca41df3051108ce82727bbd8d066d
Roberto García <roberto@rhizomik.net>
2020-05-23 10:19:41 +0200

    Set default password as env var

--- src/seeder.ts
+++ src/seeder.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from './users/users.service';
 import { User } from './users/user';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class Seeder {
 
-    constructor(private readonly userService: UsersService) {}
+    constructor(private readonly userService: UsersService,
+                private readonly configService: ConfigService) {}
 
     async seed() {
         if (!await this.userService.exists('admin')) {
-            const admin = new User({ username: 'admin', password: 'password', email: 'admin@nest.js',
-                fullname: 'Administrator', organization: 'NestJS', roles: ['admin'] });
+            const admin = new User({ username: 'admin', fullname: 'Administrator', email: 'admin@nest.js',
+                password: this.configService.get('DEFAULT_PASSWORD', 'password'),
+                organization: 'NestJS', roles: ['admin'] });
             await this.userService.create(admin);
             console.log('Creating admin user')
         }
